a01c6a9b89183ae0bd063a3e46d2265e7aaf65e0
LuisaKrawczyk <49367092+LuisaKrawczyk@users.noreply.github.com>
2019-04-23 16:37:44 +0200

    Update Manual k-means.py

--- k-means-algorithms/Manual k-means.py
+++ k-means-algorithms/Manual k-means.py
@@ -16,28 +16,28 @@ def my_kmeans(X, Y, niter_max):
   d = len(X[:,0]) # d dimensions (number of rows)
   n = len(X[0,:]) # n observations (number of columns)
   k = len(Y[0,:]) # k clusters/ centroids
-  # Nous allons stocker l'evolution de l'inertie expliquee au cours de l'algorithme.
+  # We will save the error (sum of squares) over the course of the algorithm
   Energie = np.zeros(niter_max)
   for iterations in range(niter_max): 
-    # pour chaque point du jeu de donnees, calcul du centroide qui lui est le plus proche le vecteur suivant contiendra en i l'indice du cluster le plus proche du i eme point.
+    # the following labels vector contains for each data point i the cluster that i is closest to
     labels = np.zeros(n)
-    # nombre de points assignes a chacun des clusters.
+    # number of data points assigned to each cluster
     clustersize = np.zeros(k)
     for i in range(n):
       min_distance = np.linalg.norm(X[:,i].astype(np.float64)-Y[:,0].astype(np.float64))
-      # boucle calculant les distances de i aux centroides
+      # computing the distance of i to each centroid:
       for j in range(k):
         squared_distance = np.linalg.norm(X[:,i].astype(np.float64)-Y[:,j].astype(np.float64))
         if squared_distance < min_distance: 
           min_distance = squared_distance
           labels[i]= j
         
-      # increment de la somme des residus
+      # increase of the sum of squares
       Energie[iterations] = Energie[iterations] + min_distance
-      # decompte des points appartenant au cluster
+      
       clustersize[int(labels[i])]=clustersize[int(labels[i])]+1
     
-    # calcul de la moyenne des points
+    # computing the new centroids as means/ barycentres
     Y= np.zeros((d,k))
     for i in range(n):
       Y[:,int(labels[i])]= Y[:,int(labels[i])] + X[:,i].astype(np.float64)/ clustersize[int(labels[i])]
